f4610da12fe873042e853dd81071c1de5f2f8add
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-13 12:52:03 -0700

    c

---
+++ 0x00-hello_world/0-main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void){
+ /*long com ="ls -l";*/
+ int a= 3;
+ int b= 4;
+ scanf("%d %d", &a, &b);
+ printf("%d %d \n", a, b);
+ return (0);
+}
--- 0x00-hello_world/main.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(void){
- printf("%d %d\n",3,4);
- return (0);
-}
