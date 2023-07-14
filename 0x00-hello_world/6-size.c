9c728691ec0282faf3abeffdb7a1fcbae3987643
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-14 04:18:45 -0700

    c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Sizee of various types'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("The size of an int: %d byte(s).\n", sizeof(int));
+printf("The size of a double: %d byte(s).\n", sizeof(double));
+printf("The size of a char: %d byte(s).\n", sizeof(char));
+printf("The size of a long int: %d byte(s).\n", sizeof(long int));
+printf("The size of a long long int: %d byte(s).\n", sizeof(long long int));
+printf("The size of a float: %d byte(s).\n", sizeof(float));
+return (0);
+}
