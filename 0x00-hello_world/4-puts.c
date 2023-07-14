036afd59016a2247665836c227d8257e5ef4c2ff
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-14 02:24:58 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 /*
- * int-Type of function
- * main-functiontion of code block
- * puts-Display printed text
- * return-returns back 
+* int-Type of function
+* main-functiontion of code block
+* puts-Display printed text
+* return-returns back 
 */
 int main(void)
 {
+/* statements */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
