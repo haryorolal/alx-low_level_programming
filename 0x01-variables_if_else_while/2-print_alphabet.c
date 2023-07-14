b1bded40e281475445d36af61f9ed79fab044c7b
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-14 13:14:56 -0700

    Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,7 +11,7 @@ int main(void)
 	char alpha;
         for (alpha = 'A'; alpha <= 'Z'; alpha++)
         {
-                putchar(tolower(alpha));
+                putchar(tolower(alpha),"\n");
         }
         return (0);
 }
