4c2967ada8d56b22dbebe466d5413818e3603fca
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-14 15:44:22 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Print numbers'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 0; p < 10; p++)
+	{
+		printf("%d", p);
+	}
+	putchar('\n');
+	return (0);
+
+}
