0ff5bf285dbcc6617b1660ec762e7cda527ffb08
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-18 13:06:33 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - Entry point
+ * Description: 'Checks for lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if(islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
