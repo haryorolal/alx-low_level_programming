0458f4c0354067c31c5cc845c7e5c9c9dcbbf017
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-18 14:53:55 -0700

    gsign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,14 +10,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1) + ('+');
+		return (1) | ('+');
 	}
-	else if (n ==0)
+	else if (n ==0 )
 	{
-		return (0) + (0);
+		return (0) | (0);
 	}
 	else
 	{
-		return (-1) + (-)
+		return (-1) | (-)
 	}
 }
