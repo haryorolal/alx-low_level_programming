c6d54399560042e570cc537588e51f2669ceeb55
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-18 14:22:59 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - Entry point
+ * Description: 'check if letter'
+ * @Parameter - 
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if(isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
