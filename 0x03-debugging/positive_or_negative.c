28c97ce958a3171ab9d1e010b6688ff959a58275
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-19 02:06:53 -0700

    debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/* more headers goes there */
+/**
+ * main - Entry point
+ * Description: 'Print if statement'
+ * @i: int to call
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+void positive_or_negative(int i)
+{
+        
+        srand(time(0));
+        i = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        if (i > 0)
+        {
+                printf("%d is positive\n", i);
+        }
+        else if (i == 0)
+        {
+                printf("%d is zero\n", i);
+        }
+        else
+        {
+                printf("%d is negative\n", i);
+        }
+}
