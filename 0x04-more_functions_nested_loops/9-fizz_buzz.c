456272be259af3b223fac3f43261defa8ce74d1a
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-20 09:15:32 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * print_fizz - Program that prints 1-100
+ * Description: 'Print fizz buzz'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int end_num = 100;
+
+	for (int i = 1; i <= end_num; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+			printf(" Fizz");
+		else if (i % 5 == 0 && i % 3 != 0)
+			printf(" Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf(" FizzBuzz");
+		else if (i == 1)
+			printf("%d", i);
+		else
+			printf(" %d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
