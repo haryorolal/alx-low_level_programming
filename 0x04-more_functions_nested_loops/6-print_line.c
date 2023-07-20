2ad6a02aa72e48db07f4f4d9458493c07c25d604
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-20 07:46:15 -0700

    lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,25 +1,24 @@
 #include "main.h"
-
 /**
  * print_line - function to print line
  * Description: 'To print line'
  * @n: argument to compute
  */
 void print_line(int n)
-{
-	if (n <= 0)
+{        
+        if (n <= 0)
         {
                 _putchar('\n');
         }
-	else
+        else
         {
-		int i = 0;
+                int i = 0;
 
                 while (i < n)
                 {
                         _putchar('_');
                         i++;
                 }
-		_putchar('\n');
+                _putchar('\n');
         }
 }
