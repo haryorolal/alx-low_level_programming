93c6efbfd509ac852fb3387d9509ddb58c02d176
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-20 11:25:56 -0700

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,8 +3,6 @@
 /**
  * print_numbers - function to print numbers
  * Description: 'To print numbers'
- *
- * Return: Always return 0 (Success)
  */
 void print_numbers(void)
 {
@@ -13,8 +11,8 @@ c = 0;
 
 while (c < 10)
 {
-	_putchar(c);
-	c++;
+_putchar(c);
+c++;
 }
 _putchar('\n');
 }
