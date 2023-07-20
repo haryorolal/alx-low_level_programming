f783c719503f5098e33e2c6367228bf92e440b49
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-20 11:20:42 -0700

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,13 +8,13 @@
  */
 void print_numbers(void)
 {
-	int c;
-	c = 0;
+int c;
+c = 0;
 
-	while (c < 9)
-	{
-		_putchar(c);
-		c++;
-	}
-	_putchar('\n');
+while (c < 10)
+{
+	_putchar(c);
+	c++;
+}
+_putchar('\n');
 }
