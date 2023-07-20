bc1bae0164198068db83dcbf384231eb59ebf135
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-20 03:21:44 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two arguments
+ * Description: 'Mulplies arguments'
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: Returns result of arguments
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - Entry to print
+ * Description: 'Print putchar'
+ * @c: argument to compute
+ *
+ * Return: Always return output
+ */
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
