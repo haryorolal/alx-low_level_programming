3963c8ba7c06c38ef36c04c820708b8d64545033
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-20 07:27:58 -0700

    draw lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_line - function to print line
+ * Description: 'To print line'
+ * @n: argument to compute
+ *
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+	char d = '_';
+	int i;
+	i = 0;
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < n)
+		{
+			_putchar(d);
+			i++;
+		}
+	}
+
+	_putchar('\n');
+}
