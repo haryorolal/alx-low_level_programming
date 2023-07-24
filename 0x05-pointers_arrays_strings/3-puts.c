8f834a3c99b5afa1b690addd348c2c6523ac3cc2
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-24 12:28:54 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * _puts - prints a string 
- * *str: argument to use
+ * _puts - prints a string
+ * Description: 'to prints string using put'
+ * @str: argument to use
  */
 void _puts(char *str)
 {
