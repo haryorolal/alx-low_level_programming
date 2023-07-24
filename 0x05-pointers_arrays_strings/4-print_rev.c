9c2d1ee322b06f90b203082636f9d724f3ce25f2
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-24 14:33:31 -0700

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,9 +9,10 @@ void print_rev(char *s)
 {
 int index;
 
-for (index = 0; s[index] != '\0'; ++index);
-
+for (index = 0; s[index] != '\0'; ++index)
+{
 for (--index; index >= 0; --index)
 _putchar(s[index]);
 _putchar('\n');
+}
 }
