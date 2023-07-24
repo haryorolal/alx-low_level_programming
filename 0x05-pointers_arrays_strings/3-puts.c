7196a133ed372ee077ac739c081a23b969984102
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-24 12:20:02 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - prints a string 
+ * *str: argument to use
+ */
+void _puts(char *str)
+{
+	puts(str);
+	puts("\n");
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - to return char
+ * Return: Always 0 "Success"
+ */
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
