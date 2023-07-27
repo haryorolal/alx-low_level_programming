7cfc26b1ad268585b873f9f42c854677f08ae95f
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-26 18:33:44 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * leet - function that encodes a string
+ * @b: argument
+ * Return: Always return
+ */
+char leet(char *b)
+{
+	int i, j;
+	char letter[] = "AeEeOoTtLl";
+	char num[] = "4433007711";
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (b[i] == letter[i])
+				b[i] = num[j];
+		}
+	}
+	return (s);
+}
