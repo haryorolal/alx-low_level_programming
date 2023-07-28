d0df9a90f32b3e073baf7ffc0079e71a52d907d6
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-28 08:53:39 -0700

    rot13

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	/*for (i = 0; s1[i] != '\0'; i++)
 	{
 		if (s1[i] == s2[i])
 		{
@@ -25,6 +25,25 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (s1[i] - s2[i]);
 		}
+	}
+	return (0);*/
+	i = 0;
+
+	while (s1[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+		{
+			continue;
+		}
+		else if (s1[i] >= s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		else
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
 	return (0);
 }
