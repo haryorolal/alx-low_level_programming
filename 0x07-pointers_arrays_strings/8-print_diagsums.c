677648381f5df4576a8a9474a3d4d4b475a1d120
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-31 16:09:38 -0700

    print diagsum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * print_diagsums - function to print sum of two diagonals of square
+ * @a: first argument
+ * @size: second argument
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, p, l = 0, r = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + p);
+	}
+	for (j = 0; j < size; j++)
+	{
+		p = (j * size) + (size - 1 - j);
+		r += *(a + p);
+	}
+	printf("%i, %i\n", l, r);
+}
