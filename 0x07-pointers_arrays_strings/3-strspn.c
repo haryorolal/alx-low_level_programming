341b83886d95e6328706834febcc8808d4846bdb
Ayodele Olaleye <haryorolal2@gmail.com>
2023-07-31 13:36:25 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _strspn - function that get length of a prefix
+ * @s: first argument
+ * @accept: second argument
+ * Return: Always return
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int z = 0, x, y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] != 32)
+		{
+			for (y = 0; accept[y] != '\0'; y++)
+			{
+				if (s[x] == accept[y])
+					z++;
+			}
+		}
+		else
+			return (z);
+	}
+		return (z);
+
+}
