c5ea03f6d5914fee301938f464fd5b385c255431
Ayodele Olaleye <haryorolal2@gmail.com>
2023-08-02 14:43:59 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * support_function - helper function
+ * @number: first argument
+ * @root: second argument
+ * Return: Always
+ */
+int support_function(int number, int root)
+{
+	if ((root * root) < number)
+	{
+		return (-1);
+	}
+	else if ((root * root) == number)
+	{
+		return (root);
+	}
+	else
+	{
+		return (_sqrt_recursion(number, root + 1));
+	}
+}
+
+/**
+ * _sqrt_recursion - function that returns the natural square root
+ * @n: first argument
+ * Return: Always success
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (support_function(n, 0));
+	}
+}
