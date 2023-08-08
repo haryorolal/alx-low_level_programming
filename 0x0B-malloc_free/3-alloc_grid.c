b96e50aa80a704860dfe957c4a941d49f73b41f7
Ayodele Olaleye <haryorolal2@gmail.com>
2023-08-08 08:46:22 -0700

    If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * alloc_grid - to return pointer to a 2 dimentional array of intergers
+ * @width: first argument
+ * @height: second argument
+ * Return: Always if true
+ */
+int **alloc_grid(int width, int height)
+{
+	int **newTwoDimentionalArray;
+
+	if (width == 0 || height == 0)
+	{
+		return (NULL);
+	}
+
+	newTwoDimentionalArray = malloc(sizeof(int) * height);
+
+	if (newTwoDimentionalArray != NULL)
+	{
+		int i, j;
+
+		for (i = 0; i < height; i++)
+		{
+			newTwoDimentionalArray[i] = malloc(sizeof(int) * width);
+			if (newTwoDimentionalArray[i] == NULL)
+			{
+				for (j = 0; j < i; j++)
+				{
+					free(newTwoDimentionalArray[j]);
+				}
+				free(newTwoDimentionalArray[i]);
+				return (NULL);
+			}
+
+			for (i = 0; i < width; i++)
+			{
+				newTwoDimentionalArray[i][j] = 0;
+			}
+		}
+	}
+	return newTwoDimentionalArray;
+}
