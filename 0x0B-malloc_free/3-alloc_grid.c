3fcba05f7c771a73c9fd2f1e3c1b6d2e85fd9588
Ayodele Olaleye <haryorolal2@gmail.com>
2023-08-08 09:02:21 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	newTwoDimentionalArray = malloc(sizeof(int) * height);
+	newTwoDimentionalArray = malloc(sizeof(int*) * height);
 
 	if (newTwoDimentionalArray != NULL)
 	{
