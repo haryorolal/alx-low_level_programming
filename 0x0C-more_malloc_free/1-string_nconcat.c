973fba4de876a2acf20dab3d6b05d8ab19c773db
Ayodele Olaleye <haryorolal2@gmail.com>
2023-08-10 13:24:45 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,51 +10,48 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *newS3;
-	
+
 	if (s1 == NULL || s2 == NULL)
-        {
-                return (NULL);
-        }
-	
+	{
+		return (NULL);
+	}
 	int i;
-        int s1_len = 0;
-        int s2_len = 0;
-	
+	int s1_len = 0;
+	int s2_len = 0;
+
 	while (s1[s1_len] != '\0')
-            s1_len++;
-	
+		s1_len++;
 	while (s2[s2_len] != '\0')
-            s2_len++;
-	
+		s2_len++;
+
 	newS3 = (char *)malloc(sizeof(char) * (s1_len + n + s2_len + 1));
-	
+
 	if (newS3 != NULL)
-        {
-                for (i = 0; i < s1_len; i++)
-                {
-                    newS3[i] = s1[i];
-                }
-		
+	{
+		for (i = 0; i < s1_len; i++)
+		{
+			newS3[i] = s1[i];
+		}
+
 		if (n >= s2_len)
-                {
-                    for (i = 0; i < s2_len; i++)
-                    {
-                        newS3[s1_len + i] = s2[i];
-                    }
-                }
-                else
-                {
-                    for (i = 0; i < n; i++)
-                    {
-                        newS3[s1_len + i] = s2[i];
-                    }
-                }
-		
+		{
+			for (i = 0; i < s2_len; i++)
+			{
+				newS3[s1_len + i] = s2[i];
+			}
+		}
+		else
+		{
+			for (i = 0; i < n; i++)
+			{
+				newS3[s1_len + i] = s2[i];
+			}
+		}
 		newS3[s1_len + s2_len + n] = '\0';
-        }
-        else
-        {
-            return (NULL);
-        }
+	}
+	else
+	{
+		return (NULL);
+	}
 	return (newS3);
 }
