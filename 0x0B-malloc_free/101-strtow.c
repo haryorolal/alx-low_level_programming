cb745b89921710b556386c50ae6dcd8437c5e8bc
Ayodele Olaleye <haryorolal2@gmail.com>
2023-08-09 19:39:45 -0700

    the best i can

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,151 +2,93 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * isDelimiter - function to check for empty string
- * @c: first argument
- * Return: Always
- */
-int isDelimiter(char c)
-{
-	return (c == ' ');
-}
-/**
- * countWords - function to count words
- * @str: first argument
- * Return: Always
- */
-int countWords(char *str)
-{
-	int count = 0;
-	int isWord = 0;
-
-	while (*str != '\0')
-	{
-		if (isDelimiter(*str))
-		{
-			isWord = 0;
-		}
-		else if (!isWord)
-		{
-			isWord = 1;
-			count++;
-		}
-		str++;
-	}
-
-	return (count);
-}
-/**
- * strtow - function to split strings into words
- * @str: first argument
- * Return: Always
+ * strtow - splits a string into words.
+ * @str: the string
+ *
+ * Return: returns a pointer to an array of strings (words)
  */
 char **strtow(char *str)
 {
-	int wordCount = countWords(str);
-	char **wordArray;
-	int wordIndex;
-	int isWord;
-	char *wordStart;
+	int i, flag, len;
+	char **words;
 
-	if (wordCount == 0)
-	{
+	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
 		return (NULL);
-	}
 
-	wordArray = (char **)malloc(sizeof(char *) * (wordCount + 1));
-
-	if (wordArray == NULL)
+	i = flag = len = 0;
+	while (str[i])
 	{
-		return (NULL);
-	}
-
-	wordIndex = 0;
-	isWord = 0;
-	wordStart = NULL;
-
-	while (*str != '\0')
-	{
-		if (isDelimiter(*str))
+		if (flag == 0 && str[i] != ' ')
+			flag = 1;
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
 		{
-			if (isWord)
-			{
-				int i;
-				int wordLength;
-
-				wordLength = str - wordStart;
-				wordArray[wordIndex] = (char *)malloc(wordLength + 1);
-				if (wordArray[wordIndex] == NULL)
-				{
-					while (wordIndex > 0)
-					{
-						wordIndex--;
-						free(wordArray[wordIndex]);
-					}
-
-					free(wordArray);
-					return (NULL);
-				}
-
-				for (i = 0; i < wordLength; i++)
-				{
-					wordArray[wordIndex][i] = wordStart[i];
-				}
-
-				wordArray[wordIndex][wordLength] = '\0';
-				wordIndex++;
-				isWord = 0;
-			}
+			flag = 0;
+			len++;
 		}
-		else if (!isWord)
-		{
-			isWord = 1;
-			wordStart = str;
-		}
-		str++;
-	}
-
-	if (isWord)
-	{
-		int i;
-		int wordLength;
-
-		wordLength = str - wordStart;
-		wordArray[wordIndex] = (char *)malloc(wordLength + 1);
-
-		if (wordArray[wordIndex] != NULL)
-		{
-			for (i = 0; i < wordLength; i++)
-			{
-				wordArray[wordIndex][i] = wordStart[i];
-			}
-			wordArray[wordIndex][wordLength] = '\0';
-		}
-	}
-
-	wordArray[wordCount] = NULL;
-	return (wordArray);
-}
-/**
- * freeWordArray - function to free array
- * @wordArray: first argument
- * Return: Always
- */
-void freeWordArray(char **wordArray)
-{
-	int i;
-
-	if (wordArray == NULL)
-	{
-		return;
-	}
-
-	i = 0;
-
-	while (wordArray[i] != NULL)
-	{
-		free(wordArray[i]);
 		i++;
 	}
-	free(wordArray);
+
+	len += flag == 1 ? 1 : 0;
+	if (len == 0)
+		return (NULL);
+
+	words = (char **)malloc(sizeof(char *) * (len + 1));
+	if (words == NULL)
+		return (NULL);
+
+	util(words, str);
+	words[len] = NULL;
+	return (words);
 }
+
+/**
+ * util - a util function for fetching words into an array
+ * @words: the strings array
+ * @str: the string
+ */
+void util(char **words, char *str)
+{
+	int i, j, start, flag;
+
+	i = j = flag = 0;
+	while (str[i])
+	{
+		if (flag == 0 && str[i] != ' ')
+		{
+			start = i;
+			flag = 1;
+		}
+
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+		{
+			create_word(words, str, start, i, j);
+			j++;
+			flag = 0;
+		}
+
+		i++;
+	}
+
+	if (flag == 1)
+		create_word(words, str, start, i, j);
+}
+
+/**
+ * create_word - creates a word and insert it into the array
+ * @words: the array of strings
+ * @str: the string
+ * @start: the starting index of the word
+ * @end: the stopping index of the word
+ * @index: the index of the array to insert the word
+ */
+void create_word(char **words, char *str, int start, int end, int index)
+{
+	int i, j;
+
+	i = end - start;
+	words[index] = (char *)malloc(sizeof(char) * (i + 1));
+
+	for (j = 0; start < end; start++, j++)
+		words[index][j] = str[start];
+	words[index][j] = '\0';
+}}
