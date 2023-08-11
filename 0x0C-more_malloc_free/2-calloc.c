f4c624bf7a24efeb4597fddf7b0c792173982ecc
Ayodele Olaleye <haryorolal2@gmail.com>
2023-08-11 05:24:01 -0700

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
         if (member != NULL)
         {
-		for (i = 0; i < nmemb; i++)
+		for (i = 0; i < (nmemb * size); i++)
                 {
                         member[i] = 0;
                 }
