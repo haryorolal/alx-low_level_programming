e18fd8da32cc665d8fc6bd62045913ee32ede2a1
Ayodele Olaleye <haryorolal2@gmail.com>
2023-08-11 02:07:28 -0700

    string nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,62 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+
 /**
- * string_nconcat - function that concatenates two strings
- * @s1: first argumennt
- * @s2: second argument
- * @n: third argument
- * Return: to newly allocated space in memory
+ * string_nconcat - a function that concatenates two strings.
+ *
+ * @s1: first char
+ * @s2: secound char
+ * @n: unsigned int
+ *
+ * Return: If the function fails, it should return NULL
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *newS3;
-        unsigned int s1_len;
-        unsigned int s2_len;
+	unsigned int x, y, z;
+	char *s;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
-	}
-
-	s1_len = 0;
-        s2_len = 0;
-
-	while (s1[s1_len] != '\0')
-		s1_len++;
-	while (s2[s2_len] != '\0')
-		s2_len++;
-
-	newS3 = (char *)malloc(sizeof(char) * (s1_len + n + s2_len + 1));
-
-	if (newS3 != NULL)
-	{
-		unsigned int i;
-
-		for (i = 0; i < s1_len; i++)
-		{
-			newS3[i] = s1[i];
-		}
-
-		if (n >= s2_len)
-		{
-			for (i = 0; i < s2_len; i++)
-			{
-				newS3[s1_len + i] = s2[i];
-			}
-		}
-		else
-		{
-			for (i = 0; i < n; i++)
-			{
-				newS3[s1_len + i] = s2[i];
-			}
-		}
-		newS3[s1_len + s2_len + n] = '\0';
+		x = 0;
 	}
 	else
 	{
-		return (NULL);
+		for (x = 0; s1[x]; ++x)
+		;
 	}
-	return (newS3);
+	if (s2 == NULL)
+	{
+		y = 0;
+	}
+	else
+	{
+		for (y = 0; s2[y]; ++y)
+		;
+	}
+	if (y > n)
+		y = n;
+	s = malloc(sizeof(char) * (x + y + 1));
+	if (s == NULL)
+		return (NULL);
+	for (z = 0; z < x; z++)
+		s[z] = s1[z];
+	for (z = 0; z < y; z++)
+		s[z + x] = s2[z];
+	s[x + y] = '\0';
+	return (s);
 }
