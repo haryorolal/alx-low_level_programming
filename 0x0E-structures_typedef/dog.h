dbe074174a5becd05226307e1de5ba9f62cca8de
Ayodele Olaleye <haryorolal2@gmail.com>
2023-08-14 12:35:30 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - declares variables
+ * @name: first variable
+ * @age: second variable
+ * @owner: third variable
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
