702302946cbf8d106732cc6c861e54e2ab5605ee
Ayodele Olaleye <haryorolal2@gmail.com>
2023-08-25 15:22:40 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,30 +6,22 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
-	size_t count = 0;
+	size_t i;
 
-	if (h == NULL)
-		return (0);
+	i = 0;
 
 	while (h != NULL)
 	{
-		count = 0;
-
 		if (h->str == NULL)
 		{
-			printf("[%lu] (nil)\n", count);
+			printf("[0] (nil)\n");
 		}
 		else
 		{
-			while (h->str[count] != '\0')
-			{
-				count++;
-			}
-			printf("[%lu] %s\n", count, h->str);
-			h = h->next;
-			i++;
+			printf("[%d] %s\n", h->len, h->str);
 		}
+		h = h->next;
+		i++;
 	}
 	return (i);
 }
