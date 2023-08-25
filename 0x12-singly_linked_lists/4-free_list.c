11f4903a9866b61f2798e67214bc1b27b6976132
Ayodele Olaleye <haryorolal2@gmail.com>
2023-08-25 15:04:10 -0700

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,11 +6,9 @@
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
-
-	while (head)
+	while (head != NULL)
 	{
-		temp = head;
+		list_t temp = head;
 		head = head->next;
 		free(temp->str);
 		free(temp);
