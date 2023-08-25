15ec0a8a38240cd0e1fa4d7a41d3b55f7348edc4
Ayodele Olaleye <haryorolal2@gmail.com>
2023-08-25 16:31:01 -0700

    The hare and the tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@ void printString(void) __attribute__((constructor));
  */
 void printString(void)
 {
-	printf("You're beat! and yet, you must allow\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
