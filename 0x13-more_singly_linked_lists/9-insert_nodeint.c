d1a87569b5156df150cab177abbf9f9406aa570b
Ayodele Olaleye <haryorolal2@gmail.com>
2023-08-29 18:05:20 -0700

    insert

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+/**
+ * insert_nodeint_at_index - to insert a new node at given position
+ * @head: new head
+ * @idx: index at position
+ * @n: n
+ * Return: returns address
+ */
+listint_t *insert_nodeint_at_index(listint_t **head,
+		unsigned int idx, int n)
+{
+	listint_t *newNode, *current;
+
+	newNode = malloc(sizeof(listint_t));
+
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = n;
+
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
+
+	int count = 0;
+
+	current = *head;
+	while (current != NULL)
+	{
+		if (count == idx - 1)
+		{
+			newNode = current->next;
+			current->next = newNode;
+			return (newNode);
+		}
+		count++;
+		current = current->next;
+	}
+	free(newNode);
+i	return (NULL);
+}
