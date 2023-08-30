61f3d024e56640b3b57cd9826f6206df83c5301b
Ayodele Olaleye <haryorolal2@gmail.com>
2023-08-29 18:31:08 -0700

    delee

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,28 +7,30 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *current, *temp;
+	listint_t *current, *prev;
 	unsigned int count = 0;
 
 	if (*head == NULL)
 		return (-1);
 	if (index == 0)
 	{
-		temp = *head;
+		current = *head;
 		*head = (*head)->next;
-		free(temp);
+		free(current);
 		return (1);
 	}
 	current = *head;
+	prev = NULL;
 	while (current != NULL)
 	{
 		if (count == index)
 		{
-			temp = current;
-			current = current->next;
-			free(temp);
+			prev->next = current->next;
+			free(current);
 			return (1);
 		}
+		prev = current;
+		current = current->next;
 		count++;
 	}
 	return (-1);
