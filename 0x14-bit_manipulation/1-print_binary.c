c638b21a07fea43bf26da4c075cc565fb9abc54e
Ayodele Olaleye <haryorolal2@gmail.com>
2023-08-31 14:51:42 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * print_binary - functions to print the binary representation of a number
  * @n: number
@@ -5,9 +6,12 @@
  */
 void print_binary(unsigned long int n)
 {
+	char result;
+
 	if (n > 1)
 	{
 		print_binary(n >> 1);
 	}
-	_putchar((n & 1) + '0');
+	result = (n & 1) + '0';
+	_putchar(result);
 }
