0144b39988cf2b6e7f0b8256aa6a2b6380940890
Ayodele Olaleye <haryorolal2@gmail.com>
2023-09-04 15:42:48 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,61 +1,65 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#define BUFFER_SIZE 1024
 /**
- * close_file - funciton to close file
- * @location: first agument
+ * error_exit - funciton to close file
+ * @exit_code: first argument
+ * @message: second agument
+ * @arg: third arg
  */
-void close_file(int location)
+void error_exit(int exit_code, const char *message, const char *arg)
 {
-	if (close(location) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", location);
-		exit(100);
-	}
+	dprintf(2, message, arg);
+	exit(exit_code);
 }
 /**
- * cp - function that reads a text file and prints it to POSIX
- * @file_from: file argument
- * @file_to: second file argument
+ * main - function that reads a text file and prints it to POSIX
+ * @argc: file argument
+ * @argv: second file argument
  * Return: returns 0 on success
  */
-int cp(const char *file_from, const char *file_to)
+int main(int argc, char *argv[])
 {
 	int fd_source, fd_dest;
 	ssize_t bytes_read, bytes_written;
 	char buffer[BUFFER_SIZE];
+	const char *file_from = argv[1], *file_to = argv[2];
 
-	if (file_from == NULL || file_to == NULL)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+	if (argc != 3)
+		error_exit(97, "Usage: cp file_from file_to\n", NULL);
+
 	fd_source = open(file_from, O_RDONLY);
 	if (fd_source == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+		error_exit(98, "Error: Can't read from file %s\n", file_from);
+
 	fd_dest = open(file_to, O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (fd_dest == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
+		error_exit(99, "Error: Can't write to %s\n", file_to);
+
 	bytes_read = read(fd_source, buffer, BUFFER_SIZE);
 	while (bytes_read > 0)
 	{
 		bytes_written = write(fd_dest, buffer, bytes_read);
 		if (bytes_written == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
+			error_exit(99, "Error: Can't write to %s\n", file_to);
 	}
+
 	if (bytes_read == -1)
+		error_exit(98, "Error: Can't write to %s\n", file_from);
+
+	if (close(fd_source) == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", file_from);
-		exit(98);
+		dprintf(2, "Error: Can't close fd %d\n", fd_source);
+		exit(100);
 	}
-	close_file(fd_source);
-	close_file(fd_dest);
+
+	if (close(fd_dest) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_dest);
+		exit(100);
+	}
+
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,11 +5,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
-#define BUFFER_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int cp(const char *file_from, const char *file_to);
 
 #endif
