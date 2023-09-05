08a25956f963ecb09614bc5514d58e118c528689
Ayodele Olaleye <haryorolal2@gmail.com>
2023-09-04 17:00:08 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,10 +9,10 @@
  * @message: second agument
  * @arg: third arg
  */
-void error_exit(int exit_code, const char *message, const char *arg)
+int error_exit(int exit_code, const char *message, const char *arg)
 {
 	dprintf(2, message, arg);
-	exit(exit_code);
+	return (exit_code);
 }
 /**
  * main - function that reads a text file and prints it to POSIX
@@ -28,26 +28,26 @@ int main(int argc, char *argv[])
 	const char *file_from = argv[1], *file_to = argv[2];
 
 	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to\n", NULL);
+		exit(error_exit(97, "Usage: cp file_from file_to\n", NULL));
 
 	fd_source = open(file_from, O_RDONLY);
 	if (fd_source == -1)
-		error_exit(98, "Error: Can't read from file %s\n", file_from);
+		exit(error_exit(98, "Error: Can't read from file %s\n", file_from));
 
 	fd_dest = open(file_to, O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (fd_dest == -1)
-		error_exit(99, "Error: Can't write to %s\n", file_to);
+		exit(error_exit(99, "Error: Can't write to %s\n", file_to));
 
 	while (bytes_read >= 0)
 	{
 		bytes_read = read(fd_source, buffer, BUFFER_SIZE);
 		bytes_written = write(fd_dest, buffer, bytes_read);
 		if (bytes_written == -1)
-			error_exit(99, "Error: Can't write to %s\n", file_to);
+			exit(error_exit(99, "Error: Can't write to %s\n", file_to));
 	}
 
 	if (bytes_read == -1)
-		error_exit(98, "Error: Can't write to %s\n", file_from);
+		exit(error_exit(98, "Error: Can't write to %s\n", file_from));
 
 	if (close(fd_source) == -1)
 	{
