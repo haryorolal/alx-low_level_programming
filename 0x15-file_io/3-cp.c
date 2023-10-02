909835f45ca68986f0b061d85b9f33e9fbaf6efa
Ayodele Olaleye <haryorolal2@gmail.com>
2023-10-02 07:52:19 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,15 +28,15 @@ void close_file(int d_opened_file)
  */
 int main(int argc, char *argv[])
 {
+	int file_source, file_dest, bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+	const char *file_from, *file_to;
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	int file_source, file_dest, bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
-	const char *file_from, *file_to;
 
 	file_from = argv[1];
 	file_to = argv[2];
